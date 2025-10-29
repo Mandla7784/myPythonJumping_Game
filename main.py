24f6518409985f4982fc1b86f7cf9165434a9758
madyonacpt024 <madyonacpt024@student.wethinkcode.co.za>
2025-10-29 08:27:12 +0000

    updates on server side

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import os
 
-from flask import Flask, send_file
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return send_file('src/index.html')
+    return send_from_directory("src", "index.html")
+
+
+@app.route("/<path:path>")
+def serve_static(path):
+    return send_from_directory("src", path)
+
 
 def main():
-    app.run(port=int(os.environ.get('PORT', 80)))
+    app.run(port=int(os.environ.get("PORT", 80)))
+
 
 if __name__ == "__main__":
     main()
